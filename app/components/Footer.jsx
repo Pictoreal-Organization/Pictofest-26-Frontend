157e136d5a285e6244216bf993053132dd42fc2e
ManasGawali <manasmgawali@gmail.com>
2026-01-27 12:29:41 +0530

    minor footer change

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -45,8 +45,12 @@ const Footer = () => {
           <iframe
             className="w-[75vw] h-[22vh] ml-[7vw]"
             loading="lazy"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.576189448641!2d73.84606999678954!3d18.457542099999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2eac85230ba47%3A0x871eddd0a8a0a108!2sSCTR&#39;S%20Pune%20Institute%20of%20Computer%20Technology!5e0!3m2!1sen!2sin!4v1706351618141!5m2!1sen!2sin"
-          ></iframe>
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps?q=PICT%20Pune&output=embed"
+          >
+            
+          </iframe>
+
         </div>
 
         {/* Contact */}
